80d59b6562455b446006d2fc7d1c220e5fb3308a
lalanuchie <76473023+NuchPunnawichP@users.noreply.github.com>
2024-08-15 21:46:18 +0700

    Numerical Method

---
+++ C++/0.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+// #include <iostream>
+using namespace std;
+
+void fastC();
+
+void solved()
+{
+    // Solve the problem here
+    cout << "KUY PAO";
+}
+
+main(){
+    fastC();
+    solved();
+    cout << "KUY";
+    return 0;
+}
+
+void fastC()
+{
+    ios_base :: sync_with_stdio(0);
+    cin.tie(0);
+}
