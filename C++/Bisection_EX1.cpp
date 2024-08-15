0c7d201db74a4f1944d16386812c9b6a5a2e50ad
lalanuchie <76473023+NuchPunnawichP@users.noreply.github.com>
2024-08-15 22:16:20 +0700

    ADD C++

---
+++ C++/Bisection_EX1.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+// #include <iostream>
+using namespace std;
+
+void fastC();
+
+void solved()
+{
+    // Solve the problem here
+}
+
+main(){
+    fastC();
+    solved();
+    return 0;
+}
+
+void fastC()
+{
+    ios_base :: sync_with_stdio(0);
+    cin.tie(0);
+}
